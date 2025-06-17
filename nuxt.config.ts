e994724823a2a7bae8f7c65558a3b09acbe5f68c
Kumsal Obuz <iam@denizkumsal.com>
2025-06-17 11:45:44 -0400

    install and configures tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
   typescript: {
     typeCheck: true,
     strict: false,
+  },
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
   },
   modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxt/image", "@nuxt/scripts", "@nuxt/test-utils", "@nuxt/ui"],
 });
