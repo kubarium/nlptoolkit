7c324fb49a9bf7709151aee29966bb699b2c273d
Kumsal Obuz <iam@denizkumsal.com>
2025-06-22 10:30:57 -0400

    Creates a basic home page for the project.

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<script setup lang='ts'>
+</script>
+<template>
+  <div class="flex justify-center items-center">
+    <p>The server side version of the original <a href="https://starlangsoftware.github.io/nlptoolkit-web-simple/"
+        target="_blank">NLP Toolkit</a> project.</p>
+  </div>
+</template>
