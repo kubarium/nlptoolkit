1c0ad3f06e9cf81f822e2806b3ce1333070f3eec
Kumsal Obuz <iam@denizkumsal.com>
2025-06-22 10:29:26 -0400

    Creates a reusable input area
    - The text content of the fieldset can be set when the component is deployed on different pages
    - Similarly, the button's label can be altered from the default "Find"

---
+++ components/SearchForm.vue
@@ -0,0 +1,17 @@
+<script setup lang='ts'>
+const model = defineModel<string>()
+const props = defineProps<{ buttonLabel?: string }>()
+</script>
+<template>
+  <form class="">
+    <fieldset class="border  border-zinc-200 p-2 rounded-sm ">
+      <legend class="p-2">
+        <slot />
+      </legend>
+      <UButtonGroup>
+        <UInput v-model.trim="model" required class="min-w-64" />
+        <UButton class="" type="submit" color="secondary">{{ props.buttonLabel || "Find" }}</UButton>
+      </UButtonGroup>
+    </fieldset>
+  </form>
+</template>
