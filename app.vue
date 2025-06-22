b8b34729e56735084ac26a41439169cfbb602287
Kumsal Obuz <iam@denizkumsal.com>
2025-06-22 10:26:55 -0400

    Creates a basic app structure
    - Introduces NavBar and a page side by side.
    - The NuxtPage will be swapped with the relevant route's content

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
   <UApp>
-    <NuxtPage />
+    <div class="flex flex-row h-screen">
+      <NavBar class="flex flex-col p-2 pb-0 w-72 border-r border-sky-100" />
+      <NuxtPage class="flex-grow" />
+    </div>
   </UApp>
 </template>
