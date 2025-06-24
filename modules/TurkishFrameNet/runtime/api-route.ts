59254ae6430367d47410a4ac5187f079fb9093dd
Kumsal Obuz <iam@denizkumsal.com>
2025-06-24 10:28:18 -0400

    Chooses a working function call before test deployment

--- modules/TurkishFrameNet/runtime/api-route.ts
+++ modules/TurkishFrameNet/runtime/api-route.ts
@@ -2,8 +2,8 @@ import { FrameNet } from "nlptoolkit-framenet/dist/FrameNet.js";
 const frameNet = new FrameNet();
 
 export default defineEventHandler(() => {
-  const payload = { payload: frameNet.getFrames("yemek") };
+  const payload = { payload: frameNet.getFrame(0) };
   console.log(payload);
-  
+
   return payload;
 });
