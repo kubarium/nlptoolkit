d3c4d818868e49075bc7a5689f241b9232353b53
Kumsal Obuz <iam@denizkumsal.com>
2025-06-30 12:01:12 -0400

    Adjusts the width of things for a better fit

--- components/SearchForm.vue
+++ components/SearchForm.vue
@@ -3,14 +3,14 @@ const model = defineModel<string>()
 const props = defineProps<{ buttonLabel?: string }>()
 </script>
 <template>
-  <form class="">
+  <form class="w-full max-w-1/2">
     <fieldset class="border  border-zinc-200 p-2 rounded-sm ">
       <legend class="p-2">
         <slot />
       </legend>
-      <UButtonGroup>
-        <UInput v-model.trim="model" required class="min-w-64" />
-        <UButton class="" type="submit" color="secondary">{{ props.buttonLabel || "Find" }}</UButton>
+      <UButtonGroup class="w-full">
+        <UInput v-model.trim="model" required class="w-full" />
+        <UButton class="min-w-fit" type="submit" color="secondary">{{ props.buttonLabel || "Find" }}</UButton>
       </UButtonGroup>
     </fieldset>
   </form>
