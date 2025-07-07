16d5116bbfbd38f69e74eca38a4a6ee81f87f918
Kumsal Obuz <iam@denizkumsal.com>
2025-07-07 13:02:19 -0400

    Revert "Configures the project for dynamic requires"
    This reverts commit 11046911f048e01e50647dcc02a7b45f48583e28.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from "@tailwindcss/vite";
-import commonjs from "@rollup/plugin-commonjs";
 
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
@@ -12,7 +11,7 @@ export default defineNuxtConfig({
   },
   css: ["~/assets/css/main.css"],
   vite: {
-    plugins: [commonjs({ dynamicRequireTargets: ["node_modules/nlptoolkit-corpus/**"] }), tailwindcss()],
+    plugins: [tailwindcss()],
   },
   modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxt/image", "@nuxt/scripts", "@nuxt/test-utils", "@nuxt/ui"],
 });
