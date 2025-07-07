11046911f048e01e50647dcc02a7b45f48583e28
Kumsal Obuz <iam@denizkumsal.com>
2025-07-07 12:38:11 -0400

    Configures the project for dynamic requires

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from "@tailwindcss/vite";
+import commonjs from "@rollup/plugin-commonjs";
 
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
@@ -11,7 +12,7 @@ export default defineNuxtConfig({
   },
   css: ["~/assets/css/main.css"],
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [commonjs({ dynamicRequireTargets: ["node_modules/nlptoolkit-corpus/**"] }), tailwindcss()],
   },
   modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxt/image", "@nuxt/scripts", "@nuxt/test-utils", "@nuxt/ui"],
 });
