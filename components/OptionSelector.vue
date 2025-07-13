9b0615597bbf48c6f99801f983fd1c946ac6c545
Kumsal Obuz <iam@denizkumsal.com>
2025-07-13 17:06:21 -0400

    Introduces a generic option selector
    The first use will be exposing available POS taggers. Most likely, the next candidate will be deasciifiers

---
+++ components/OptionSelector.vue
@@ -0,0 +1,14 @@
+<script setup lang='ts'>
+const model = defineModel<string>()
+const props = defineProps<{ items: string[] }>()
+</script>
+<template>
+  <form class="">
+    <fieldset class="border  border-zinc-200 p-2 rounded-sm ">
+      <legend class="p-2">
+        <slot />
+      </legend>
+      <USelect v-model="model" :items="props.items" class="w-full" />
+    </fieldset>
+  </form>
+</template>
